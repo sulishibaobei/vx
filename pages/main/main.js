72fd63654242b34dc604fa07d694d8a6fe4f9160
sulishibaobei <380571249@qq.com>
2018-11-24 17:49:03 +0800

    11.24

--- pages/main/main.js
+++ pages/main/main.js
@@ -6,18 +6,11 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    greet:'This’s my first little program',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   locationViewTap: function () {
     wx.navigateTo({
       url: '../map/map'
