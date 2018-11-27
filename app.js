a19e6c2835798d308656c8d1cf4e32dd08b9109d
sulishibaobei <380571249@qq.com>
2018-11-27 18:32:39 +0800

    1

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
 //app.js
+/**
+ * 将请求封装成promise
+ */
+const wechat = require('./utils/wechat.js');
+/**
+ * 为了根据经纬度获取地址
+ */
+const baidu = require('./utils/baidu.js');
+/**
+ * doubanAPI模块
+ */
+const douban = require('./utils/douban.js')
 App({
-  onLaunch: function (options) {
-    // 展示本地存储能力
-    // var logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    //  wx.setStorageSync('logs', logs)
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
+    wechat: wechat,
+    baidu: baidu,
+    douban: douban,
+    data: {
+        currentCity: "北京"
+    },
+    onLaunch: function(options) {
+        // 展示本地存储能力
+        // var logs = wx.getStorageSync('logs') || []
+        // logs.unshift(Date.now())
+        //  wx.setStorageSync('logs', logs)
+        // 登录
+        wx.login({
+                success: res => {
+                    // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                }
+            })
+            // 获取用户信息
+        wx.getSetting({
             success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            // 可以将 res 发送给后台解码出 unionId
+                            this.globalData.userInfo = res.userInfo
+                                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                                // 所以此处加入 callback 以防止这种情况
+                            if (this.userInfoReadyCallback) {
+                                this.userInfoReadyCallback(res)
+                            }
+                        }
+                    })
+                }
             }
-          })
-        }
-      }
-    })
-  },
-  onError:function(error){
-    wx.setStorageSync('logs', error);
-  }, 
-  globalData: {
-    userInfo: null
-  }
+        })
+    },
+    onError: function(error) {
+        wx.setStorageSync('logs', error);
+    },
+    globalData: {
+        userInfo: null
+    }
 })
--- douban/splash/splash.js
+++ douban/splash/splash.js
@@ -1,23 +1,62 @@
-// douban/splash/splash.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+const app = getApp()
+Page({
+    data: {
+        movies: [],
+        name: 'Douban Movie',
+        version: '0.1.0',
+        currentCity: '北京',
+        loading: true
+    },
+    onLoad: function() {
+        var that = this;
+        app.wechat.getLocation()
+            .then(res => {
+                const { latitude, longitude } = res
+                return app.baidu.getCityName(latitude, longitude)
+            })
+            .then(name => {
+                that.data.currentCity = name.replace('市', '')
+                console.log(`currentCity : ${this.data.currentCity}`)
+            })
+            .catch(err => {
+                that.data.currentCity = '北京'
+                console.error(err)
+            });
+        this.getCache()
+            .then(cache => {
+                if (cache) {
+                    return this.setData({ movies: cache.movies, loading: false })
+                }
+                app.douban.find('coming_soon', 1, 3)
+                    .then(d => {
+                        this.setData({ movies: d.subjects, loading: false })
+                        return app.wechat.setStorage('last_splash_data', {
+                            movies: d.subjects,
+                            expires: Date.now() + 1 * 24 * 60 * 60 * 1000
+                        })
+                    })
+                    .then(() => console.log('storage last splash data'))
+            })
+    },
+    getCache() {
+        return new Promise(resolve => {
+            app.wechat.getStorage('last_splash_data')
+                .then(res => {
+                    const { movies, expires } = res.data
+                        // 有缓存，判断是否过期
+                    if (movies && expires > Date.now()) {
+                        return resolve(res.data)
+                    }
+                    // 已经过期
+                    console.log('uncached')
+                    return resolve(null)
+                })
+                .catch(e => resolve(null))
+        })
+    },
+    startExperience: function() {
+        wx.navigateTo({
+            url: '/douban/board/board'
+        })
+    }
 })
